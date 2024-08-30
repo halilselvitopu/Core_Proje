e114cc1aff52f780b8a3e7c4ed6f00915ef46978
halilselvitopu <halilselvitopu@gmail.com>
2024-08-30 04:53:52 +0300

    weatherAPI and Dashboard

---
+++ Core_Proje/Areas/Writer/Controllers/DashboardController.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+
+    [Area("Writer")]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task <IActionResult> Index()
+        {
+
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v1 = values.Name + " " + values.Surname;
+
+            //weather API
+
+            string api = "2907bfa2c5551a927caa7f36339b5be0";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v6 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+            //statistics
+
+            Context c = new Context();
+            ViewBag.v2 = 0;
+            ViewBag.v3 = c.Announcements.Count();
+            ViewBag.v4 = 0;
+            ViewBag.v5 = c.Skills.Count();
+            return View();
+        }
+    }
+}
+
+/*
+  https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=2907bfa2c5551a927caa7f36339b5be0
+ */
